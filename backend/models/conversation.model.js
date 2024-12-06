bb4fda9f6ca087ee0c5ee802ae4a9fa7a9e0a1f2
hitmanGoGo <hitmangogo7@gmail.com>
2024-12-06 23:37:44 +0530

    update

---
+++ backend/models/conversation.model.js
@@ -0,0 +1,23 @@
+import mongoose, { mongo } from "mongoose";
+
+const conversationSchema = new mongoose.Schema(
+    {
+        participants: [
+            {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: "user",
+            },
+        ],
+        messages: [
+            {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: "Message",
+            },
+        ],
+    },
+    { timestamps: true}
+);
+
+const Conversation = mongoose.model("Conversation", conversationSchema);
+
+export default Conversation;
--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,20 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import cookieParser from "cookie-parser";
 
-import authRoutes from './routes/auth.routes.js'
+import authRoutes from './routes/auth.routes.js';
+import messageRoutes from './routes/message.routes.js';
 import connectToMongoDB from './db/connectToMongoDB.js';
 
 const app = express();
 
 dotenv.config();
 app.use(express.json());
+app.use(cookieParser);
 const PORT = process.env.PORT || 5001;
 
 app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
 
 app.listen(PORT, () => {
     connectToMongoDB();
